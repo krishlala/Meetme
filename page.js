9b61c7f4006feda86d2d7e4523bfca675f1295d2
krishlala <71400038+krishlala@users.noreply.github.com>
2021-07-21 13:14:46 -0400

    Update page.js

--- page.js
+++ page.js
@@ -27,7 +27,8 @@ function send()
   document.getElementById("msg").value = "";
 }
 
-function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+function getData() {
+	firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
