0c66116e657872762d03aa43c9178e7559929116
krishlala <71400038+krishlala@users.noreply.github.com>
2021-07-23 15:09:26 -0400

    Update page.js

--- page.js
+++ page.js
@@ -17,7 +17,7 @@ function send()
 {
   msg = document.getElementById("msg").value;
   firebase.database().ref(room_name).push({
-	console.log("it worked!")
+	console.log("it worked!");
     name:user_name,
     message:msg,
     like:0
