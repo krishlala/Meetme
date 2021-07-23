d734ef3c75248ffe71f4d9a6dcc5d6377bb10cad
krishlala <71400038+krishlala@users.noreply.github.com>
2021-07-23 13:44:43 -0400

    Update page.js

--- page.js
+++ page.js
@@ -10,8 +10,6 @@
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-
-
 	user_name = localStorage.getItem("user_name");
 	room_name = localStorage.getItem("room_name");
 
@@ -27,8 +25,7 @@ function send()
   document.getElementById("msg").value = "";
 }
 
-function getData() {
-	firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
