191dbce9efeb7abfe9746a2dfe3db890bb2bb981
krishlala <71400038+krishlala@users.noreply.github.com>
2021-07-26 09:07:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,67 +1,27 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDVI4pvt_MEqVLosXOq1espiDLis9a9d-Q",
-    authDomain: "chatroomreal.firebaseapp.com",
-    databaseURL: "https://chatroomreal-default-rtdb.firebaseio.com",
-    projectId: "chatroomreal",
-    storageBucket: "chatroomreal.appspot.com",
-    messagingSenderId: "585545969401",
-    appId: "1:585545969401:web:2e9656779d6c0a1d857c28"
+    apiKey: "AIzaSyAdaUxS5pIAvy0yjJIL2scAT6qsgZEHmDw",
+    authDomain: "login-8d8c3.firebaseapp.com",
+    projectId: "login-8d8c3",
+    storageBucket: "login-8d8c3.appspot.com",
+    messagingSenderId: "664217504416",
+    appId: "1:664217504416:web:1c0bd64189f1778b6d17c5"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-	user_name = localStorage.getItem("user_name");
-	room_name = localStorage.getItem("room_name");
 
-function send()
-{
-  console.log(msg);
-  msg = document.getElementById("msg").value;
-  firebase.database().ref(room_name).push({
-    name:user_name,
-    message:msg,
-    like:0
-   });
 
-  document.getElementById("msg").value = "";
-}
-
-function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
-         firebase_message_id = childKey;
-         message_data = childData;
-//Start code
-         console.log(firebase_message_id);
-	       console.log(message_data);
-	       name = message_data['name'];
-	       message = message_data['message'];
-         like = message_data['like'];
-         name_with_tag = "<h4> "+ name +"<img class='user_tick' src='IMG_0043.jpeg'></h4>";
-         message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
-like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
-         span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
-
-        row = name_with_tag + message_with_tag +like_button + span_with_tag;       
-        document.getElementById("output").innerHTML += row;
-//End code
-      } });  }); }
-getData();
-
-function updateLike(message_id)
-{
-  console.log("clicked on like button - " + message_id);
-	button_id = message_id;
-	likes = document.getElementById(button_id).value;
-	updated_likes = Number(likes) + 1;
-	console.log(updated_likes);
-
-	firebase.database().ref(room_name).child(message_id).update({
-		like : updated_likes  
-	 });
+function start(){
+    window.location.replace("choose.html");
 
 }
 
-function logout() {
-localStorage.removeItem("user_name");
-localStorage.removeItem("room_name");
-window.location.replace("index.html");
+function choose(){
+    window.location.replace("choose.html");
+
+}
+
+function quiz(){
+    window.location.replace("quiz.html");
+
 }
