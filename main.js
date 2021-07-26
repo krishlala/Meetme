3405354e979986146f69d191fdf167f4ee61ed5d
krishlala <71400038+krishlala@users.noreply.github.com>
2021-07-26 11:48:59 -0400

    Update main.js

--- main.js
+++ main.js
@@ -982,3 +982,17 @@ function addUser() {
 }
 
 
+
+
+window.addEventListener('load', function() {
+  document.querySelector('input[type="file"]').addEventListener('change', function() {
+      if (this.files && this.files[0]) {
+          var img = document.querySelector('img');
+          img.onload = () => {
+              URL.revokeObjectURL(img.src);  // no longer needed, free memory
+          }
+
+          img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+      }
+  });
+});
