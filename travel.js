406e6fe0c433d7fd7ac869eded1b9335eaf57b46
krishlala <71400038+krishlala@users.noreply.github.com>
2021-07-26 09:10:41 -0400

    Update travel.js

--- travel.js
+++ travel.js
@@ -1,3 +1,31 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAdaUxS5pIAvy0yjJIL2scAT6qsgZEHmDw",
+    authDomain: "login-8d8c3.firebaseapp.com",
+    projectId: "login-8d8c3",
+    storageBucket: "login-8d8c3.appspot.com",
+    messagingSenderId: "664217504416",
+    appId: "1:664217504416:web:1c0bd64189f1778b6d17c5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+function start(){
+    window.location.replace("choose.html");
+
+}
+
+function choose(){
+    window.location.replace("choose.html");
+
+}
+
+function quiz(){
+    window.location.replace("quiz.html");
+
+}
+
 function addUser() {
 
 
