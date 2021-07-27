2feefac46547130e009eb95698b17120bbaafcf5
krishlala <71400038+krishlala@users.noreply.github.com>
2021-07-27 18:19:39 -0400

    Update page.js

--- page.js
+++ page.js
@@ -74,7 +74,7 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
 	       name = message_data['name'];
 	       message = message_data['message'];
          like = message_data['like'];
-         name_with_tag = "<h4> "+ name +"<img class='user_tick' src="+#imageUpload+"></h4>";
+         name_with_tag = "<h4> "+ name +"<img class='user_tick' src="+this+"></h4>";
          message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
 like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
          span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
