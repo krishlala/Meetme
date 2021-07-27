39c13d4917f92eea56287e9e327902b75f451978
krishlala <71400038+krishlala@users.noreply.github.com>
2021-07-27 18:00:19 -0400

    Update page.js

--- page.js
+++ page.js
@@ -51,6 +51,20 @@ function send()
   document.getElementById("msg").value = "";
 }
 
+$("#profileImage").click(function(e) {
+    $("#imageUpload").click();
+});
+
+function fasterPreview( uploader ) {
+    if ( uploader.files && uploader.files[0] ){
+          $('#profileImage').attr('src', 
+             window.URL.createObjectURL(uploader.files[0]) );
+    }
+}
+$("#imageUpload").change(function(){
+    fasterPreview( this );
+});
+
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
          message_data = childData;
@@ -60,7 +74,7 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
 	       name = message_data['name'];
 	       message = message_data['message'];
          like = message_data['like'];
-         name_with_tag = "<h4> "+ name +"<img class='user_tick' src='tick.png'></h4>";
+         name_with_tag = "<h4> "+ name +"<img class='user_tick' src="+#imageUpload+"></h4>";
          message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
 like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
          span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
