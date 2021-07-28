675059ac751c5423b07d589872cc1c1652f28632
krishlala <71400038+krishlala@users.noreply.github.com>
2021-07-28 15:46:37 -0400

    Update page.js

--- page.js
+++ page.js
@@ -65,7 +65,7 @@ $("#imageUpload").change(function(){
     fasterPreview( this );
 });
 
-var profileimg = document.getElementById("imageUpload").value;
+var profileimg = document.getElementById("profileImage").src;
 
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
@@ -76,7 +76,7 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
 	       name = message_data['name'];
 	       message = message_data['message'];
          like = message_data['like'];
-         name_with_tag = "<h4> "+ name +"<img class='user_tick' src="+profileImage+"></h4>";
+         name_with_tag = "<h4> "+ name +"<img class='user_tick' src="+profileimg+"></h4>";
          message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
 like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
          span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
